f0adb674d054e5e3306cb149c5302b4c0f718a28
Tijan <tijanmdr@gmail.com>
2016-08-26 19:06:35 +0545

    Added Controllers: TaskController & NotesController Added Migrations for Notes and Tasks

---
+++ database/migrations/2016_08_26_131703_create_task_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('task_id');
+            $table->string('task_title');
+            $table->text('task_details');
+            $table->timestamps();
+            $table->timestamp('edited_on');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
