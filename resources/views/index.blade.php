3c574948fa1cccc8637b7377aacd2c16571f1437
Tijan <tijanmdr@gmail.com>
2016-08-27 15:01:15 +0545

    Signed-off-by: Tijan <tijanmdr@gmail.com>

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,20 @@
 	@include('partials.menu')
 	<div class="clear"></div>
 	<hr>
-	<h3>Welcome</h3>
+	<div class="col-md-6 col-sm-6 col-xs-12 col-lg-6 menulist menulistbordered">
+		<h3>Tasks</h3>
+		<hr>
+		<h4><a href="/create-note">Create a Note</a></h4>
+		<hr class="brokeLine">
+		<h4><a href="/notes">Show all Notes</a></h4>
+	</div>
+	<div class="col-md-6 col-sm-6 col-xs-12 col-lg-6 menulist">
+		<h3>Notes</h3>
+		<hr>
+		<h4><a href="/create-task">Create a Task</a></h4>
+		<hr class="brokeLine">
+		<h4><a href="/tasks">Show all Task</a></h4>
+	</div>
+	<div class="clear"></div>
 	<hr>
 @stop
--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,4 +1,5 @@
 <ul class="nav nav-tabs col-md-6 col-sm-6 col-xs-12 col-lg-6">
-	<li><a href="#">Tasks</a></li>
-	<li><a href="#">Notes</a></li>
+	<li><a href="/">Home</a></li>
+	<li><a href="/tasks">Tasks</a></li>
+	<li><a href="/notes">Notes</a></li>
 </ul>
